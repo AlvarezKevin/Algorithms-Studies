e161e50eaacebd5b6e29d08a0150b62f8c1ca552
Kevin Alvarez <kevinalvarez1025@gmail.com>
2017-06-05 20:08:01 -0400

    Moved sorting to own method

--- src/Sorting_Implementations/SelectionSort.java
+++ src/Sorting_Implementations/SelectionSort.java
@@ -16,27 +16,40 @@ public class SelectionSort {
         System.out.println("Before selection sort: \n" + Arrays.toString(a));
 
         long startTime = System.currentTimeMillis();
+
+        a = selectionSort(a);
+
+        long endTime = System.currentTimeMillis();
+        long time = endTime - startTime;
+
+        System.out.println("After selection sort: \n" + Arrays.toString(a));
+        System.out.println("Time: " + time);
+
+    }
+
+    public static int[] selectionSort(int[] a) {
+        //Selection Sort O(n^2)
         long n = 0;
-        //Trying to implement selection sort
+
         for (int i = 0; i < a.length; i++) {
+
             int minPos = i;
+
             for (int j = i + 1; j < a.length; j++) {
                 if (a[j] < a[minPos]) {
                     minPos = j;
                 }
                 n++;
             }
+
             int temp = a[i];
             a[i] = a[minPos];
             a[minPos] = temp;
+
             n++;
         }
-
-        long endTime = System.currentTimeMillis();
-        long time = endTime - startTime;
-        System.out.println("After selection sort: \n" + Arrays.toString(a));
-        System.out.println("Time: " + time);
         System.out.println("N: " + n);
+        return a;
     }
 }
 
