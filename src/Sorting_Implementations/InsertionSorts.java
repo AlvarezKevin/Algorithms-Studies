d28a0e21584020046855c931469fd5132af804e5
Kevin Alvarez <kevinalvarez1025@gmail.com>
2017-06-06 21:19:01 -0400

    Moved sorting to its own method

--- src/Sorting_Implementations/InsertionSorts.java
+++ src/Sorting_Implementations/InsertionSorts.java
@@ -8,34 +8,43 @@ import java.util.Random;
  */
 public class InsertionSorts {
     public static void main(String[] args) {
-        int[] a = new int[100000];
-        for (int i = 0; i < 100000; i++) {
-            a[i] = new Random().nextInt(1000000) + 1;
+        int[] a = new int[10];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = new Random().nextInt(10) + 1;
         }
 
         System.out.println("Before insertion sort: \n" + Arrays.toString(a));
 
         long startTime = System.currentTimeMillis();
-        long n = 0;
 
-        for(int i = 1;i < a.length;i++) {
-            int key = a[i];
-
-            int j = i - 1;
-            while(j >= 0 && a[j] > key) {
-                a[j + 1] = a[j];
-                j--;
-                n++;
-            }
-            a[j + 1] = key;
-            n++;
-        }
+        a = insertionSort(a);
 
         long endTime = System.currentTimeMillis();
         long time = endTime - startTime;
 
         System.out.println("After insertion sort: \n" + Arrays.toString(a));
         System.out.println("Time: " + time);
+
+    }
+
+    public static int[] insertionSort(int[] a) {
+        long n = 0;
+
+        for (int i = 1; i < a.length; i++) {
+            int key = a[i];
+
+            int j = i - 1;
+            while (j >= 0 && a[j] > key) {
+                a[j + 1] = a[j];
+                j--;
+                n++;
+            }
+            a[j + 1] = key;
+
+            n++;
+        }
+
         System.out.println("N: " + n);
+        return a;
     }
 }
