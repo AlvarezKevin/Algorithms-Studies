65cc94ea9d0f1ec49e7146be9e134a9266165d74
Kevin Alvarez <kevinalvarez1025@gmail.com>
2017-06-13 10:33:06 -0400

    Solved Question 1.1 from CTCI

---
+++ src/CTCI_Chapter1_Arrays_Strings/IsUnique_1_1.java
@@ -0,0 +1,27 @@
+package CTCI_Chapter1_Arrays_Strings;
+
+import java.util.Scanner;
+
+/**
+ * Created by kevin on 6/13/2017.
+ */
+public class IsUnique_1_1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Input a word");
+        String word = input.nextLine();
+
+        System.out.println("Is string " + word + " unique? Answer is " + stringUnique(word));
+    }
+    public static boolean stringUnique(String word) {
+        int[] alpha = new int[128];
+        for(int i = 0;i < word.length();i++) {
+            int temp = word.charAt(i);
+            alpha[temp]++;
+            if(alpha[i] > 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
